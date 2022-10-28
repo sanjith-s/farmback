31725cffea7b5a5cbdd001e99cfe8d83df6bb1db
harshit0902 <83470228+harshit0902@users.noreply.github.com>
2022-10-28 21:28:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const morgan = require("morgan");
 const mongoose = require("mongoose"); 
 const app = express();
 const dbURI = 'mongodb+srv://Farmenience:Farm123@cluster0.ocxfoad.mongodb.net/?retryWrites=true&w=majority';
-mpngoose.connect(dbURI, {useNewUrlParser:true, useUnifiedTopology:true})
+mongoose.connect(dbURI, {useNewUrlParser:true, useUnifiedTopology:true})
     .then((result) => app.listen(3001,()=>{
         console.log("Server is running");
     })
-    .catch((err) => console.log(err));
-)
+    .catch((err) => console.log(err)));
