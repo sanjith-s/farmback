e3661415a7882ccab85d3e48960b8c614c664133
r.navaneeth992 <r.navaneeth992@gmail.com>
2022-10-30 00:45:15 +0530

    Login and Signup Page Completed
    Signup and Login Backend completed
    Just did as we do for basic backend
    Afterwards we can change to whatever i discussed in meet

--- index.js
+++ index.js
@@ -1,10 +1,68 @@
 const express = require("express");
-const morgan = require("morgan");
 const mongoose = require("mongoose"); 
 const app = express();
+app.use(express.json())
+const Credentials = require("./models/credentials")
 const dbURI = 'mongodb+srv://Farmenience:Farm123@cluster0.ocxfoad.mongodb.net/?retryWrites=true&w=majority';
-mongoose.connect(dbURI, {useNewUrlParser:true, useUnifiedTopology:true})
-    .then((result) => app.listen(3001,()=>{
-        console.log("Server is running");
+app.listen(3001,()=>{
+    console.log("Server is running");
+});
+async function connect() {
+    try {
+      mongoose.connect(dbURI);
+      console.log("Connected to MongoDB");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+app.post("/check",(req,res)=>{
+    res.send( { message: "postman check.",err:null})
+})
+connect();
+app.post("/signup", (req, res)=> {
+    console.log(req.body.name);
+    let str1={ email: req.body.email};
+    let email=req.body.email;
+    Credentials.findOne(str1, (user) => {
+        if(user){
+            res.send({message: "User already registered",err:null})
+        } else {
+            const user = new Credentials({
+                name:req.body.name, 
+                phoneno:req.body.phoneno,
+                aadhaarno:req.body.aadhaarno,
+                addline1:req.body.addline1,
+                addline2:req.body.addline2,
+                city:req.body.city,
+                district:req.body.district,
+                pincode:req.body.pincode,
+                email:req.body.email,
+                password:req.body.password
+            })
+            user.save(err => {
+                if(err) {
+                    res.send({message:"", err:err})
+                } else {
+                    res.send( { message: "Successfully Registered, Please login now.",err:null})
+                }
+            })
+        }
+    })  
+})
+app.get("/login", (req, res)=> {
+    const {password} = req.body;
+    const email=req.body.email;
+    console.log(email);
+    let str1={ email: email};
+    Credentials.findOne(str1, (err, user) => {
+        if(user){
+            if(password === user.password ) {
+                res.send({message: "Login Successfull", name: user.name})
+            } else {
+                res.send({ message: "Password didn't match",name: null})
+            }
+        } else {
+            res.send({message: "User not registered",name: null})
+        }
     })
-    .catch((err) => console.log(err)));
+})
