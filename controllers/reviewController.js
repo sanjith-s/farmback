f4952c675e359561efabc0e543c214e126a1efd2
sanjith-s <sanjithsenthilmurugan@gmail.com>
2023-04-17 16:44:25 +0530

    Review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,25 +1,34 @@
 const Review = require("../models/reviews");
-const Users=require("../models/credentials");
+const Users = require("../models/credentials");
+const { sendReview } = require("../utilities/sendReview");
 
-const postReview = async(req, res) => {
-    try{
-        let email=res.locals.details;
-        const profile = await Users.findOne({email:email});
-        const query = new Review({
-            name:profile.name,
-            email:res.locals.details,
-            phonenum:profile.phoneno,
-            review:req.body.review,
-        })
-        
-        await query.save();
-        console.log("After Saving Query");
-        res.status(201).json({message: "Review added !!"});
-    } catch {
-        res.status(404).json({message:"Error in Connection."});
-    }
-}
+const postReview = async (req, res) => {
+  try {
+    let email = req.body.email;
+    const profile = await Users.findOne({ email: email });
+    const query = new Review({
+      name: profile.name,
+      email: req.body.email,
+      phonenum: profile.phoneno,
+      review: req.body.review,
+    });
+
+    await query.save();
+
+    console.log("After Saving Query");
+
+    sendReview(email, {
+      name: profile.name,
+      number: profile.phoneno,
+      review: req.body.review,
+    });
+
+    res.status(201).json({ message: "Review added !!" });
+  } catch {
+    res.status(404).json({ message: "Error in Connection." });
+  }
+};
 
 module.exports = {
-    postReview,
-}
+  postReview,
+};
